70274c4478d3fa1046516608fe40e37127688086
guguping <qkfhandur@naver.com>
2023-04-21 15:49:30 +0900

    day42_README_test7

--- src/main/java/com/icia/ex2/controller/HomeController.java
+++ src/main/java/com/icia/ex2/controller/HomeController.java
@@ -1,14 +1,14 @@
 package com.icia.ex2.controller;
 
 import com.icia.ex2.dto.StudentDTO;
-import org.apache.ibatis.annotations.Delete;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+
 import java.util.ArrayList;
 import java.util.List;
 
